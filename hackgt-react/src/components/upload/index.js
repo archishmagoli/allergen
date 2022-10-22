3f454abb0cf9e95560654163eeba97c56d246e6e
Saniya Savla <ssaniyasavla@gmail.com>
2022-10-22 13:35:01 -0400

    added upload feature

--- hackgt-react/src/components/upload/index.js
+++ hackgt-react/src/components/upload/index.js
@@ -1,17 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import "./upload.css";
 
 function Upload() {
+  const [allergy, setAllergy] = useState("");
+  const [nutritionLabel, setNutritionLabel] = useState(null);
   return (
     <div class="form-container">
       <form class="upload-box">
-      <h1>Is your food safe to eat?</h1>
+      <h1 id="form-label">Is your food safe to eat?</h1>
         <input
           id="allergy"
           class="form-field"
           type="text"
           placeholder="Allergy"
           name="allergy"
+          value={allergy}
+          onChange={(e) => setAllergy(e.target.value)}
         />
         <input
           id="nutritional-label"
@@ -19,6 +23,8 @@ function Upload() {
           type="file"
           placeholder="Nutritional Label"
           name="nutritionallabel"
+          value={nutritionLabel}
+          onChange={(e)=>setNutritionLabel(e.target.value)}
         />
         <button class="form-field" type="submit" id="submit-btn">
           Submit
@@ -28,30 +34,3 @@ function Upload() {
   );
 }
 export default Upload;
-// import React, {useState} from 'react'
-// import './upload.css'
-// function Upload() {
-//     const [allergy, setAllergy] = useState("");
-//     const [nutritionLabel, setNutritionLabel] = useState(null);
-//     return (
-//         <div className="upload">
-//             <div className='upload-container'>
-//                 <div className='upload-box'>
-//                     <form className='upload-form'>
-//                         Allergy
-//                         <input type="text" name = "allergy" value={allergy}
-//                             onChange={(e) => setAllergy(e.target.value)}/>
-//                         <br/>
-//                         Nutritional Label
-//                         <input type="file" name = "nutrition" value={nutritionLabel}
-//                 onChange={(e) => setNutritionLabel(e.target.value)}/>
-//                         <br/>
-//                         <input type="submit" />
-//                     </form>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// }
-
-// export default Upload;
