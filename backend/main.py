140d9faf48b26a277a57a520c7002aea78348773
archishmagoli <agoli6@gatech.edu>
2024-05-16 18:20:03 -0400

    working on ocr

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,22 @@
 from flask import Flask, request
 from flask_cors import CORS
+from PIL import Image
+import pytesseract
 
 app = Flask(__name__)
 CORS(app) # allow for cross-origin requests (from the frontend)
 
 @app.route('/process', methods=['POST'])
 def api():
-    data = request.form['allergies']
+    allergies = request.form['allergies']
+    pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
-    return data
+    # Get the image from the request
+    image = request.files['image']
+    image = Image.open(image)
+    text = pytesseract.image_to_string(image)
+
+    return 'Hi'
 
 
 if __name__ == '__main__':
