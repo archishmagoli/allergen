cc1b8d850bb04494c3f7b0093fabbdc34bad743c
archishmagoli <agoli6@gatech.edu>
2024-05-18 16:13:15 -0400

    add fuzzy search

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 from PIL import Image
 import pytesseract
 from textblob import TextBlob
+from fuzzywuzzy import process
 
 app = Flask(__name__)
 CORS(app) # allow for cross-origin requests (from the frontend)
@@ -10,6 +11,9 @@ CORS(app) # allow for cross-origin requests (from the frontend)
 @app.route('/process', methods=['POST'])
 def api():
     allergies = request.form['allergies']
+    allergies = allergies.split(',')
+    allergies = [allergy.strip() for allergy in allergies]
+
     pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
     # Get the image from the request
@@ -35,9 +39,36 @@ def api():
     corrected_text = blob.correct()
 
     allergens_in_label = []
+    for allergen in allergies:
+        if fuzzy_search(allergen, corrected_text):
+            allergens_in_label.append(allergen)
     
-    return 'Hi'
+    if len(allergens_in_label) > 0:
+        return ', '.join(allergens_in_label)
+    else:
+        return ''
 
+
+def fuzzy_search(query, string_list, threshold=80):
+    """
+    Perform fuzzy search to check if the query matches any item in the string list.
+    
+    Args:
+    - query (str): The query string to search for.
+    - string_list (list): The list of strings to search within.
+    - threshold (int): The threshold for the fuzzy match (default is 80).
+    
+    Returns:
+    - bool: True if a fuzzy match is found, False otherwise.
+    """
+    # Get the best match from the string list
+    best_match, score = process.extractOne(query, string_list)
+    
+    # Check if the match score is above the threshold
+    if score >= threshold:
+        return True
+    else:
+        return False
 
 if __name__ == '__main__':
     app.run(debug=True)
